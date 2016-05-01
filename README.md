876b5e2b0431e7b69ee634df6117ff99b42f3f16
Joey Andres <jandres@ualberta.ca>
2016-04-30 19:54:53 -0600

    Basic

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # template-assert-parent
 A simple package that help enforces template's parent template.
+
+## Installation
+```bash
+meteor add meteoric124:template-assert-parent
+```
+
+## To use:
+Suppose you have two templates: `foo` and `bar`. And suppose you want
+`bar` to be parent of `foo`, then in `foo`'s `onRendered` callback:
+
+```javascript
+Template.foo.onRendered(function() {
+  this.assertParent(Template.bar);
+});
+```
+
+If `foo` is not under `bar`, then in your console screen:
+
+> Template.foo template must be under Template.bar
+
+## Caveats
+
+This is all the use-case **I** need for the moment. That is, I don't care if
+it is an immediate child or grand* child. If you need to be able to specify
+levels, ask me, and I'll push something.
---
+++ package.js
@@ -0,0 +1,24 @@
+Package.describe({
+    name: 'meteoric124:template-assert-parent',
+    version: '0.1.0-alpha.1',
+    // Brief, one-line summary of the package.
+    summary: 'A simple package that help enforces template\'s parent template.',
+    // URL to the Git repository containing the source code for this package.
+    git: 'https://github.com/meteoric124/template-assert-parent.git',
+    // By default, Meteor will default to using README.md for documentation.
+    // To avoid submitting documentation, set this field to null.
+    documentation: 'README.md'
+});
+
+var package_client_dependencies = [
+    "meteoric124:template-scope@0.1.0-beta.4",
+    'underscore',
+    'templating'
+];
+
+Package.onUse(function(api) {
+    api.versionsFrom('1.3.2.4');
+    api.use([ 'ecmascript' ]);
+    api.use(package_client_dependencies, 'client');
+    api.mainModule('lib/main.js', 'client');
+});
